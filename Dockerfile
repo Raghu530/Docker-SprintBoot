6a853922a0f03b38cb6b4eabe956c75a4a56a4f1
rxred18 <rxred18@5CG44506FQ.ctl.intranet>
2018-08-16 19:02:09 +0530

    Updated FIle

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8-jdk-alpine
-ADD resources\docker-spring-boot.jar docker-spring-boot.jar
+ADD \docker-spring-boot.jar docker-spring-boot.jar
 EXPOSE 8085
 ENTRYPOINT ["java", "-jar", "/docker-spring-boot.jar"]
--- src/main/java/com/ctl/docker/example/HelloResource.java
+++ src/main/java/com/ctl/docker/example/HelloResource.java
@@ -1,14 +1,13 @@
-package com.ctl.docker.example.resource;
+package com.ctl.docker.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rest/docker/hello")
+
 public class HelloResource {
 	
-	@GetMapping
+	@RequestMapping("/rest/docker/hello")
 	public String hello() {
 		return "Hello Docker Example";
 	}
